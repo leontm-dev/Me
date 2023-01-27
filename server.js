1833204b196f90126a1c5e209c6d9ceb4b982245
Leon <LAnneken09@gmail.com>
2023-01-27 20:03:11 +0100

    App Port

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 const express = require("express");
 const app = express();
 const env = require("dotenv");
+const cors = require("cors");
+// App Einstellungen
+app.listen(2000, () => {
+    fs.readFile("./console.txt", function(err, data) {
+        console.log(String(data));
+    });
+});
