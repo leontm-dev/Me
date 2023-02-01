a4abe6b2c28cb48f41dc8bfd0c46481b75fb7d8c
Leon <LAnneken09@gmail.com>
2023-02-01 20:09:52 +0100

    Fertigstellung der JavaScript Datei für die PopUps

---
+++ Seiten/public/Alle/popup.js
@@ -0,0 +1,11 @@
+// Elemente
+const popupBackground = document.getElementById("popup-background");
+
+function allCookies() {
+    console.log("Cookie Auswahl wurde bestätigt. Anzahl der Cookies: 0");
+    popupBackground.classList.add("hide");
+};
+function noCookies() {
+    console.log("Cookie Auswahl wurde bestätigt. Anzahl der Cookies: 0");
+    popupBackground.classList.add("hide");
+};
