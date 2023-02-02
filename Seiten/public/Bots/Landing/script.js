307d22cc0b2a591b8c383a394bc438ed1664c48e
Leon <LAnneken09@gmail.com>
2023-02-02 20:37:12 +0100

    Added: functions signUpDiscord() and scrollToNextSection()

---
+++ Seiten/public/Bots/Landing/script.js
@@ -0,0 +1,7 @@
+// Funktionen
+function signUpDiscord() {
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1070781003519500399&redirect_uri=http%3A%2F%2Flocalhost%3A2000%2Fbots%2Fconnected%2F&response_type=token&scope=identify%20guilds%20email%20guilds.join%20connections%20guilds.members.read%20messages.read";
+};
+function scrollToNextSection() {
+    window.scrollBy({"behavior": "smooth", "top": 1920 * 100})
+}
