57ac254a6a59cd762092eba5aea2d7ca7f45198f
Leon <LAnneken09@gmail.com>
2023-02-02 20:37:31 +0100

    Fixes

---
+++ Seiten/public/Index/Start/script.js
@@ -0,0 +1,6 @@
+function startPresentation() {
+    window.location.href = `${window.location.href}folie=1`;
+};
+function skipPresentation() {
+    window.location.href = `${window.location.href}summary`;
+}
