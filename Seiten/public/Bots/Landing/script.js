700c81d356571e40e477be5bf81f176cc45d8e50
Leon <LAnneken09@gmail.com>
2023-02-07 20:10:37 +0100

    Changed: Discord Auth URL

--- Seiten/public/Bots/Landing/script.js
+++ Seiten/public/Bots/Landing/script.js
@@ -1,6 +1,6 @@
 // Funktionen
 function signUpDiscord() {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1070781003519500399&redirect_uri=https%3A%2F%2FMe.leontm-official.repl.co%2Fbots%2Fconnected&response_type=token&scope=identify%20guilds%20dm_channels.read%20email%20guilds.join%20connections%20guilds.members.read%20messages.read";
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=1070781003519500399&redirect_uri=https%3A%2F%2FMe.leontm-official.repl.co%2Fbots%2Fconnected&response_type=token&scope=identify%20guilds%20guilds.join%20connections%20guilds.members.read";
 };
 function scrollToNextSection() {
     window.scrollBy({"behavior": "smooth", "top": window.innerHeight})
