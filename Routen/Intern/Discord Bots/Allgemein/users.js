de060deb6165b8fa2397cf3c96b905c47274e9f4
Leon <LAnneken09@gmail.com>
2023-02-07 20:52:05 +0100

    Added: File Code

---
+++ Routen/Intern/Discord Bots/Allgemein/users.js
@@ -0,0 +1,35 @@
+// Datei Einstellungen
+const express = require("express");
+const editJSON = require("edit-json-file");
+const fs = require("fs");
+// Router Einstellungen
+const router = express.Router();
+// Datenbank Einstellungen
+const USER = editJSON("Daten/Discord/Bots/User/user.json");
+// Routen
+router.post("/users/new/:id/:token", (req, res) => {
+    if (USER.get(String(req.params.id)) != undefined) {
+        res.status(403).json({
+            code: 403,
+            message: "User already exists"
+        })
+    } else {
+        fetch("discord.com/api/v10/users/@me", {method: "GET", headers: { Authorization: `Bearer ${req.params.token}`}})
+        .then(res => {
+            if (res.status == 200) {
+                return res.json();
+            }
+        })
+        .then(data => {
+            USER.set(String(req.params.id), data);
+            USER.set(`${String(req.params.id)}.token`, String(req.params.token));
+            USER.save();
+            res.status(200).json({
+                code: 200,
+                message: "User set!"
+            })
+        })
+    }
+})
+// Exports
+module.exports = router
