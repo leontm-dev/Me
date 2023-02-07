c7278854ef6ccb74026e7c6a1b24bdc1ad10b5dd
Leon <LAnneken09@gmail.com>
2023-02-07 20:30:30 +0100

    Added: File Code

---
+++ Seiten/public/Bots/Connected/script.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+    if (window.location.href.includes("#token_type=Bearer&access_token_")) {
+        let TOKEN = window.location.href.replace("https://me.leontm-official.repl.co/bots/connected#token_type=Bearer&access_token=", "").replace("&expires_in=604800&scope=guilds.join+guilds.members.read+guilds+connections+identify", "");
+        fetch("https://discord.com/api/v10/users/@me",
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${TOKEN}`
+                }
+            }
+        ).then(res => {
+            if (res.status == 200) {
+                return res.json();
+            } else {
+                window.location.href = "https://Me.leontm-official.repl.co/bots";
+            }
+        })
+        .then(data => {
+            console.log(data.id);
+            let ID = data.id
+            fetch("",
+                {
+                    method: "GET",
+                    headers: {
+                        authorization: "NJ295Kix9M88ghaDVFqYrtQ72P3lXR"
+                    }
+                }
+            )
+            .then(res => {
+                if (res.status == 200) {
+                    return res.json()
+                } else {
+                    window.location.href = "https://Me.leontm-official.repl.co/bots"
+                }
+            })
+            .then(data => {
+                window.location.href = `https://Me.leontm-official.repl.co/bots/accounts/${ID}`;
+            })
+        })
+    }
+}
