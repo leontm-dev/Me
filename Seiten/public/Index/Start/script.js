c29928f164bebf48d7ac21a13e7ebfc535f0279f
Leon <LAnneken09@gmail.com>
2023-02-13 18:57:32 +0100

    Removed

--- Seiten/public/Index/Start/script.js
+++
@@ -1,6 +0,0 @@
-function startPresentation() {
-    window.location.href = `${window.location.href}folie=1`;
-};
-function skipPresentation() {
-    window.location.href = `${window.location.href}summary`;
-}
